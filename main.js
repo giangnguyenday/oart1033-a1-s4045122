0439a3e0a52ad4db922c19f1168a25fe66fda7f5
Giang Nguyen <135529655+giangnguyenday@users.noreply.github.com>
2024-08-11 16:57:27 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,321 @@
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry)
+    if (entry.isIntersecting){
+        entry.target.classList.add('show');
+    } else {
+      entry.target.classList.remove('show');
+    }
+  })
+})
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
+
+/*==2==*/
+const observer2 = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry)
+    if (entry.isIntersecting){
+        entry.target.classList.add('show2');
+    } else {
+      entry.target.classList.remove('show2');
+    }
+  })
+})
+
+const hiddenElements2 = document.querySelectorAll('.hidden2');
+hiddenElements2.forEach((el) => observer2.observe(el));
+
+
+/*==3==*/
+const observer3 = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry)
+    if (entry.isIntersecting){
+        entry.target.classList.add('show3');
+    } else {
+      entry.target.classList.remove('show3');
+    }
+  })
+})
+
+const hiddenElements3 = document.querySelectorAll('.hidden3');
+hiddenElements3.forEach((el) => observer3.observe(el));
+
+/*==4==*/
+const observer4 = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry)
+    if (entry.isIntersecting){
+        entry.target.classList.add('show4');
+    } else {
+      entry.target.classList.remove('show4');
+    }
+  })
+})
+
+const hiddenElements4 = document.querySelectorAll('.hidden4');
+hiddenElements4.forEach((el) => observer4.observe(el));
+
+/*==5==*/
+const observer5 = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry)
+    if (entry.isIntersecting){
+        entry.target.classList.add('show5');
+    } else {
+      entry.target.classList.remove('show5');
+    }
+  })
+})
+
+const hiddenElements5 = document.querySelectorAll('.hidden5');
+hiddenElements5.forEach((el) => observer5.observe(el));
+
+/*==6==*/
+const observer6 = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry)
+    if (entry.isIntersecting){
+        entry.target.classList.add('show6');
+    } else {
+      entry.target.classList.remove('show6');
+    }
+  })
+})
+
+const hiddenElements6 = document.querySelectorAll('.hidden6');
+hiddenElements6.forEach((el) => observer6.observe(el));
+
+/*==7==*/
+const observer7 = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry)
+    if (entry.isIntersecting){
+        entry.target.classList.add('show7');
+    } else {
+      entry.target.classList.remove('show7');
+    }
+  })
+})
+
+const hiddenElements7 = document.querySelectorAll('.hidden7');
+hiddenElements7.forEach((el) => observer7.observe(el));
+
+
+/*==8==*/
+const observer8 = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry)
+    if (entry.isIntersecting){
+        entry.target.classList.add('show8');
+    } else {
+      entry.target.classList.remove('show8');
+    }
+  })
+})
+
+const hiddenElements8 = document.querySelectorAll('.hidden8');
+hiddenElements8.forEach((el) => observer8.observe(el));
+
+/*==9==*/
+const observer9 = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry)
+    if (entry.isIntersecting){
+        entry.target.classList.add('show9');
+    } else {
+      entry.target.classList.remove('show9');
+    }
+  })
+})
+
+const hiddenElements9 = document.querySelectorAll('.hidden9');
+hiddenElements9.forEach((el) => observer9.observe(el));
+
+/*==10==*/
+const observer10 = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry)
+    if (entry.isIntersecting){
+        entry.target.classList.add('show10');
+    } else {
+      entry.target.classList.remove('show10');
+    }
+  })
+})
+
+const hiddenElements10 = document.querySelectorAll('.hidden10');
+hiddenElements10.forEach((el) => observer10.observe(el));
+
+/*==11==*/
+const observer11 = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry)
+    if (entry.isIntersecting){
+        entry.target.classList.add('show11');
+    } else {
+      entry.target.classList.remove('show11');
+    }
+  })
+})
+
+const hiddenElements11 = document.querySelectorAll('.hidden11');
+hiddenElements11.forEach((el) => observer11.observe(el));
+
+/*==12==*/
+const observer12 = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry)
+    if (entry.isIntersecting){
+        entry.target.classList.add('show12');
+    } else {
+      entry.target.classList.remove('show12');
+    }
+  })
+})
+
+const hiddenElements12 = document.querySelectorAll('.hidden12');
+hiddenElements12.forEach((el) => observer12.observe(el));
+
+/*==13==*/
+const observer13 = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry)
+    if (entry.isIntersecting){
+        entry.target.classList.add('show13');
+    } else {
+      entry.target.classList.remove('show13');
+    }
+  })
+})
+
+const hiddenElements13 = document.querySelectorAll('.hidden13');
+hiddenElements13.forEach((el) => observer13.observe(el));
+
+/*==14==*/
+const observer14 = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry)
+    if (entry.isIntersecting){
+        entry.target.classList.add('show14');
+    } else {
+      entry.target.classList.remove('show14');
+    }
+  })
+})
+
+const hiddenElements14 = document.querySelectorAll('.hidden14');
+hiddenElements14.forEach((el) => observer14.observe(el));
+
+/*==15==*/
+const observer15 = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry)
+    if (entry.isIntersecting){
+        entry.target.classList.add('show15');
+    } else {
+      entry.target.classList.remove('show15');
+    }
+  })
+})
+const hiddenElements15 = document.querySelectorAll('.hidden15');
+hiddenElements15.forEach((el) => observer15.observe(el));
+
+/*==16==*/
+const observer16 = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry)
+    if (entry.isIntersecting){
+        entry.target.classList.add('show16');
+    } else {
+      entry.target.classList.remove('show16');
+    }
+  })
+})
+
+const hiddenElements16 = document.querySelectorAll('.hidden16');
+hiddenElements16.forEach((el) => observer16.observe(el));
+
+/*==17==*/
+const observer17 = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry)
+    if (entry.isIntersecting){
+        entry.target.classList.add('show17');
+    } else {
+      entry.target.classList.remove('show17');
+    }
+  })
+})
+
+const hiddenElements17 = document.querySelectorAll('.hidden17');
+hiddenElements17.forEach((el) => observer17.observe(el));
+
+/*==18==*/
+const observer18 = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry)
+    if (entry.isIntersecting){
+        entry.target.classList.add('show18');
+    } else {
+      entry.target.classList.remove('show18');
+    }
+  })
+})
+
+const hiddenElements18 = document.querySelectorAll('.hidden18');
+hiddenElements18.forEach((el) => observer18.observe(el));
+
+/*==19==*/
+const observer19 = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry)
+    if (entry.isIntersecting){
+        entry.target.classList.add('show19');
+    } else {
+      entry.target.classList.remove('show19');
+    }
+  })
+})
+
+const hiddenElements19 = document.querySelectorAll('.hidden19');
+hiddenElements19.forEach((el) => observer19.observe(el));
+
+/*==20==*/
+const observer20 = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry)
+    if (entry.isIntersecting){
+        entry.target.classList.add('show20');
+    } else {
+      entry.target.classList.remove('show20');
+    }
+  })
+})
+
+const hiddenElements20 = document.querySelectorAll('.hidden20');
+hiddenElements20.forEach((el) => observer20.observe(el));
+
+/*=====*/
+let sections = document.querySelectorAll('section');
+
+window.onscroll = () => {
+  sections.forEach(sec => {
+    let top = window.scrollY;
+    let offset  = sec.offsetTop;
+    let height = sec.offsetHeight;
+
+    if (top >= offset && top < offset + height) {
+      sec.classList.add('show-animate');
+    }
+    else{
+      sec.classList.remove('show-animate');
+
+    }
+  })
+}
+
+
+
